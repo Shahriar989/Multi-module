5dfcba2fb8b793a738dbfb9371e8772be38bcf7c
Shahriar <https://github.com/Shahriar989/E-Cart.git>
2023-01-29 22:59:48 +0600

    init

--- app/src/main/java/com/shahriar/a13_multimodule_class_38/MainActivity.kt
+++ app/src/main/java/com/shahriar/a13_multimodule_class_38/MainActivity.kt
@@ -2,10 +2,21 @@ package com.shahriar.a13_multimodule_class_38
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shahriar.a13_multimodule_class_38.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (this::binding.isInitialized) {
+            setContentView(binding.root)
+        } else {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+        }
+
+
     }
 }
